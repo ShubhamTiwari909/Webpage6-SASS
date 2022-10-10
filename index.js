60e5323212959abf03ca20ba475365abe7b13e18
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-10-10 10:28:49 +0530

    added carousel

--- index.js
+++ index.js
@@ -14,69 +14,10 @@ const cardLogo = document.querySelectorAll('.review-card-logo');
 const dots = document.querySelectorAll('.dot');
 
 
-const slider = (count) => {
-    for (let index = 0; index < cards.length; index++) {
-        if (index === count) {
-            cards[index].style.backgroundColor = "#1fdb84";
-            cards[index].style.color = "white";
-            cardText[index].style.color = "white";
-            cardUserText[index].style.color = "white";
-            cardLogo[index].src = "https://dl.dropboxusercontent.com/s/jrvy4nec2hyxtgh/group-6.png?dl=0"
-            dots[index].style.backgroundColor = "#1fdb84"
-        }
-        else {
-            cards[index].style.backgroundColor = "white";
-            cardUserText[index].style.color = "#031d5b";
-            cardText[index].style.color = "#5b6c94";
-            cardLogo[index].src = "https://dl.dropboxusercontent.com/s/4mw6ondhuf2mv0s/group-7.png?dl=0"
-            dots[index].style.backgroundColor = "transparent"
-
-        }
-        if (window.innerWidth >= 300) {
-            cards[index].style.left = `-${count * 350}px`;
-        }
-        if (window.innerWidth >= 800) {
-            cards[index].style.left = `-${count * 400}px`;
-        }
-        if (window.innerWidth >= 801 && window.innerWidth < 1000) {
-            cards[index].style.left = `-${count * 300}px`;
-        }
-    }
-    cards.forEach((element,index) => {
-        element.style.transform = `translateX(${index * 10}px)`
-    });
-}
-
-const handleMouseIn = value => {
-    for (let i = 0; i < pricingCard.length; i++) {
-        if (i === value) {
-            pricingCard[i].style.backgroundColor = "#fff";
-            pricingCard[i].style.borderTop = "3px solid #0a8aff";
-            pricingCard[i].style.boxShadow = "0 7px 24px 0 rgba(83, 83, 83, 0.1)";
-        }
-        else {
-            pricingCard[i].style.backgroundColor = "transparent";
-            pricingCard[i].style.borderTop = "none";
-            pricingCard[i].style.boxShadow = "0 0 0 0 rgba(83, 83, 83, 0.1)";
-        }
-    }
-};
-
-const handleMouseOut = () => {
-    for (let i = 0; i < pricingCard.length; i++) {
-        pricingCard[i].style.backgroundColor = "transparent";
-        pricingCard[i].style.borderTop = "none";
-        pricingCard[i].style.boxShadow = "0 0 0 0 rgba(83, 83, 83, 0.1)";
-    }
-    scrolledNavigation();
-}
 
 
 
-
-
-
-
+// Navbar dropdown effect function
 const dropdownNavigation = () => {
     if (navigation.style.display === "none" && navButtons.style.display === "none") {
         header.style.height = "102vh";
@@ -125,6 +66,79 @@ const dropdownNavigation = () => {
 }
 
 
+
+
+
+
+// Pricing card hover effect function
+const handleMouseIn = value => {
+    for (let i = 0; i < pricingCard.length; i++) {
+        if (i === value) {
+            pricingCard[i].style.backgroundColor = "#fff";
+            pricingCard[i].style.borderTop = "3px solid #0a8aff";
+            pricingCard[i].style.boxShadow = "0 7px 24px 0 rgba(83, 83, 83, 0.1)";
+        }
+        else {
+            pricingCard[i].style.backgroundColor = "transparent";
+            pricingCard[i].style.borderTop = "none";
+            pricingCard[i].style.boxShadow = "0 0 0 0 rgba(83, 83, 83, 0.1)";
+        }
+    }
+};
+
+const handleMouseOut = () => {
+    for (let i = 0; i < pricingCard.length; i++) {
+        pricingCard[i].style.backgroundColor = "transparent";
+        pricingCard[i].style.borderTop = "none";
+        pricingCard[i].style.boxShadow = "0 0 0 0 rgba(83, 83, 83, 0.1)";
+    }
+    scrolledNavigation();
+}
+
+
+
+
+
+// Carousel slider function
+const slider = (count) => {
+    for (let index = 0; index < cards.length; index++) {
+        if (index === count) {
+            cards[index].style.backgroundColor = "#1fdb84";
+            cards[index].style.color = "white";
+            cardText[index].style.color = "white";
+            cardUserText[index].style.color = "white";
+            cardLogo[index].src = "https://dl.dropboxusercontent.com/s/jrvy4nec2hyxtgh/group-6.png?dl=0"
+            dots[index].style.backgroundColor = "#1fdb84"
+        }
+        else {
+            cards[index].style.backgroundColor = "white";
+            cardUserText[index].style.color = "#031d5b";
+            cardText[index].style.color = "#5b6c94";
+            cardLogo[index].src = "https://dl.dropboxusercontent.com/s/4mw6ondhuf2mv0s/group-7.png?dl=0"
+            dots[index].style.backgroundColor = "transparent"
+
+        }
+        if (window.innerWidth >= 300) {
+            cards[index].style.left = `-${count * 350}px`;
+        }
+        if (window.innerWidth >= 800) {
+            cards[index].style.left = `-${count * 400}px`;
+        }
+        if (window.innerWidth >= 801 && window.innerWidth < 1000) {
+            cards[index].style.left = `-${count * 300}px`;
+        }
+    }
+    cards.forEach((element,index) => {
+        element.style.transform = `translateX(${index * 10}px)`
+    });
+}
+
+
+
+
+
+
+// changing stylings when the viewport changes just like media queries
 function changingMediaQuery() {
     header.style.height = "0";
     header.style.backgroundColor = "transparent";
@@ -156,6 +170,11 @@ function changingMediaQuery() {
 
 window.addEventListener('resize', changingMediaQuery, slider);
 
+
+
+
+
+// Changing navbar styling when the scroll height is more than or equal 100
 let scrolledNavigation = () => {
     "use strict";
     if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100) {
